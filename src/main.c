1f236d02d3d8e43b6dfc8e9e6854c1dcef8a5271
KMNA-projects <rajameena.v@gmail.com>
2026-07-06 13:21:04 +0200

    - Added few code

--- src/main.c
+++ src/main.c
@@ -1,11 +1,56 @@
 #include <stdio.h>
 #include "math_utils.h"
 
-int main(void)
+/*int main(void)
 {
     int result = add(10, 20)
 
     printf("10 + 20 = %d\n", result);
 
     return 0;
+}*/
+/*
+int main()
+{
+	printf("Hello World");
+
+	int i,j;
+	for(i=0; i<10; i++)
+	{
+		if (i == 4)
+			i = i +3;
+		printf("%d", i);
+	}
+
+	return 0;
+}
+-*/
+
+/*
+int main ()
+{
+    int i = 5;
+    while( i--)
+    {
+        printf("%d", i);
+    }
+}
+-*/
+/*
+int main ()
+{
+    int j = 10;
+    int *p = &j;
+    *p = 20;
+    printf("%d%d", j,*p);
+}
+
+*/
+
+#include <stdio.h>
+int main(){
+    int arr[]={10,20,30,40,50};
+    int *p=arr+1;
+    printf("%x\n", *p);
+    printf("%d %d %d\n", *p, *(p-1), *(p+1));
 }
